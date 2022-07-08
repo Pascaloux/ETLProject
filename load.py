4b31c77a2dd8e688a25862b092cb64ae534e43c8
Pascale Caloux <107123249+Pascaloux@users.noreply.github.com>
2022-07-08 11:27:24 +0200

    Mise à jour du fichier load.py

--- load.py
+++ load.py
@@ -1,9 +1,18 @@
 from sqlalchemy import *
 from sqlalchemy_utils import *
 from sqlalchemy.orm import *
+from table import *
 
 
 def load(df):
     print("Chargement des valeurs foncières")
-    test=df.to_dict('records')
-    print(test)
+    result=df.to_dict('records')
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for item in result:
+        row = ValeursFoncieres(**item)
+        session.add(row)
+
+    session.commit()
