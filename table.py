71dd4083e480dff017607b3169d768619b5bcc5a
lindayasmina <mbeutchang@gmail.com>
2022-07-12 15:23:50 +0200

    Update table.py

--- table.py
+++ table.py
@@ -9,8 +9,8 @@ class DateValeursFoncieres(Base):
     DateMAJ       = Column(String(20),primary_key=True)
 
 
-class ValeursFoncieres(Base):
-    __tablename__ = "ValeursFoncieres"
+class ValeursFoncieres_temp(Base):
+    __tablename__ = "ValeursFoncieres_temp"
     IdValeurFonciere        = Column(Integer,primary_key=True)
     NoDisposition           = Column(Integer)
     DateMutation            = Column(Date)
@@ -47,6 +47,66 @@ class ValeursFoncieres(Base):
     NatureCulture           = Column(String(2))
     NatureCultureSpeciale   = Column(String(5))
     SurfaceTerrain          = Column(Integer)
+    transaction_id = column_property(cast(ValeurFonciere,String)+ " " 
+    + cast(DateMutation,String) + " " 
+    + cast(NoVoie,String) + " " 
+    + BTQ  + " " 
+    + TypeVoie + " " 
+    + CodeVoie + " " 
+    + Voie + " " 
+    + cast(CodePostal,String) + " "
+    + Commune + " " 
+    + CodeDepartement )
+
+class ValeursFoncieres_final(Base):
+    __tablename__ = "ValeursFoncieres_final"
+    IdValeurFonciere        = Column(Integer,primary_key=True)
+    NoDisposition           = Column(Integer)
+    DateMutation            = Column(Date)
+    NatureMutation          = Column(String(40))
+    ValeurFonciere          = Column(Float)
+    NoVoie                  = Column(Integer)
+    BTQ                     = Column(String(1))
+    TypeVoie                = Column(String(4))
+    CodeVoie                = Column(String(4))
+    Voie                    = Column(String(100))
+    CodePostal              = Column(Integer)
+    Commune                 = Column(String(45))
+    CodeDepartement         = Column(String(3))
+    CodeCommune             = Column(Integer)
+    PrefixeSection          = Column(Integer)
+    Section                 = Column(String(2))
+    NoPlan                  = Column(Integer)
+    NoVolume                = Column(String(10))
+    Lot1                    = Column(String(10))
+    CarrezLot1              = Column(Float)
+    Lot2                    = Column(String(10))
+    CarrezLot2              = Column(Float)
+    Lot3                    = Column(String(10))
+    CarrezLot3              = Column(Float)
+    Lot4                    = Column(String(10))
+    CarrezLot4              = Column(Float)
+    Lot5                    = Column(String(10))
+    CarrezLot5              = Column(Float)
+    NombreLots              = Column(Integer)
+    CodeTypeLocal           = Column(Integer)
+    TypeLocal               = Column(String(25))
+    SurfaceReelleBati       = Column(Integer)
+    NombrePiecesPrincipales = Column(Integer)
+    NatureCulture           = Column(String(2))
+    NatureCultureSpeciale   = Column(String(5))
+    SurfaceTerrain          = Column(Integer)
+    transaction_id = column_property(cast(ValeurFonciere,String)+ " " 
+    + cast(DateMutation,String) + " " 
+    + cast(NoVoie,String) + " " 
+    + BTQ  + " " 
+    + TypeVoie + " " 
+    + CodeVoie + " " 
+    + Voie + " " 
+    + cast(CodePostal,String) + " "
+    + Commune + " " 
+    + CodeDepartement )
+
 
 
 class Bank_cap_temp(Base):
@@ -66,14 +126,14 @@ class Bank_cap_final(Base):
     identifier = column_property(Name + " " +cast( Market_Cap,String))
 
 class Taux_temp(Base):
-    __tablename__ = "taux"
+    __tablename__ = "taux_temp"
     id = Column(Integer,primary_key=True)
     devise= Column(String(4))
     valeur = Column(Float)
     transaction_id= column_property(devise+" "+cast(valeur,String))
 
 class Taux_final(Base):
-    __tablename__ = "taux"
+    __tablename__ = "taux_final"
     id = Column(Integer,primary_key=True)
     devise= Column(String(4))
     valeur = Column(Float)
