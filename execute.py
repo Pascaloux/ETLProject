8692bc3ecfddbd080a83d68aef36263812ce1e61
lindayasmina <mbeutchang@gmail.com>
2022-07-13 16:29:25 +0200

    Update execute.py

--- execute.py
+++ execute.py
@@ -5,10 +5,13 @@ import load
 # Extraction des données
 date = extract.extract_date_valeurs_foncieres()
 df_raw  = extract.extract_valeurs_foncieres()
+df_raw_capitaux = extract.extract_capitaux()
 
 #Transformation des données
 df_prep = transform.transform_valeurs_foncieres(df_raw)
+df_prep_capitaux=transform.transform_capitaux(df_raw_capitaux,df_taux)
 
 # Chargement des données
 load.load_date_valeurs_foncieres(date)
 load.load_valeurs_foncieres(df_prep)
+load.load_valeurs_foncieres(df_prep_capitaux)
